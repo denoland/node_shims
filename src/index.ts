cbdc553c028eeb8b2b1b0ee5215e23ef87d324b2
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-05-01 22:16:39 +0200

    Make types honest

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-///<reference path="lib.deno.d.ts" />
-
-import * as _Deno from "./deno.js";
-
-//@ts-expect-error
-export const Deno: typeof globalThis.Deno = _Deno;
+export * as Deno from "./deno.js";
