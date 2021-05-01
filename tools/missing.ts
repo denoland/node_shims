58103a7ed34609ae7b749c1fdca42b4e1c7f0d97
Thomas Rory Gummerson <thomas@gummerson.no>
2021-05-01 23:36:14 +0200

    Add counting to missing.ts

--- tools/missing.ts
+++ tools/missing.ts
@@ -15,10 +15,19 @@ const wontAdd = new Set([
   "Buffer",
 ]);
 
+const count = wontAdd.size;
+const total = Object.keys(Deno).length;
+
+let added = 0;
 for await (const dirEntry of Deno.readDir("src/deno")) {
   wontAdd.add(dirEntry.name.slice(0, -3));
+  added++;
 }
 
 for (const key of Object.keys(Deno).sort()) {
   if (!wontAdd.has(key)) console.log(key);
 }
+
+console.log(
+  `${added} (remaining: ${total - added}) / ${total - count} (total: ${total})`,
+);
