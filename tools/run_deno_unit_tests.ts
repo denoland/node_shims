dd67d1114135cba1cbf133306f63a15466393c9a
Thomas Rory Gummerson <thomas@gummerson.no>
2021-05-06 16:00:50 +0200

    Testing bugfixes

--- tools/run_deno_unit_tests.ts
+++ tools/run_deno_unit_tests.ts
@@ -7,12 +7,14 @@ const ignore = new Set([
   "body_test.ts",
   "buffer_test.ts",
   // "build_test.ts",
+  "build_test.ts",
   "chmod_test.ts",
   "chown_test.ts",
   "console_test.ts",
   "copy_file_test.ts",
   "custom_event_test.ts",
   "dir_test.ts",
+  "dispatch_test.ts",
   "error_stack_test.ts",
   "event_target_test.ts",
   "event_test.ts",
@@ -20,6 +22,7 @@ const ignore = new Set([
   "filereader_test.ts",
   "files_test.ts",
   "file_test.ts",
+  "filter_function_test.ts",
   "format_error_test.ts",
   "fs_events_test.ts",
   // "get_random_values_test.ts",
@@ -33,7 +36,6 @@ const ignore = new Set([
   "metrics_test.ts",
   "mkdir_test.ts",
   "net_test.ts",
-  "opcall_test.ts",
   "os_test.ts",
   "path_from_url_test.ts",
   "performance_test.ts",
@@ -65,6 +67,8 @@ const ignore = new Set([
   "truncate_test.ts",
   "tty_test.ts",
   "umask_test.ts",
+  "unit_tests.ts",
+  "unit_test_runner.ts",
   "url_search_params_test.ts",
   "url_test.ts",
   "utime_test.ts",
@@ -76,7 +80,7 @@ const ignore = new Set([
   "write_text_file_test.ts",
 ]);
 
-for await (const e of Deno.readDir("deno/cli/tests/unit")) {
+for await (const e of Deno.readDir("vendor/deno/cli/tests/unit")) {
   if (e.isFile && e.name.endsWith(".ts") && !ignore.has(e.name)) {
     await Deno.run({
       cmd: [
@@ -86,7 +90,7 @@ for await (const e of Deno.readDir("deno/cli/tests/unit")) {
         "--allow-write=unit",
         "--unstable",
         "https://raw.githubusercontent.com/trgwii/Nodeify/master/nodeify.ts",
-        `deno/cli/tests/unit/${e.name}`,
+        `vendor/deno/cli/tests/unit/${e.name}`,
         "unit",
       ],
       stdin: "null",
