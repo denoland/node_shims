3ffe119321f20b23a827cfe3744ee22f11daece3
Muthu Kumar <muthukumar@thefeathers.in>
2021-06-15 19:48:20 +0530

    (fix) node gc's FileHandle returned by fsPromises
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- src/deno/stable/functions/open.ts
+++ src/deno/stable/functions/open.ts
@@ -1,9 +1,12 @@
 ///<reference path="../lib.deno.d.ts" />
 
-import { open as nodeOpen } from "fs/promises";
+import { open as _open } from "fs";
+import { promisify } from "util";
 
 import { File } from "../classes/File";
 import { getFsFlag } from "../../internal/fs_flags";
+
+const nodeOpen = promisify(_open);
 
 export const open: typeof Deno.open = async function open(
   path,
@@ -17,6 +20,6 @@ export const open: typeof Deno.open = async function open(
     create,
     createNew,
   });
-  const f = await nodeOpen(path, flagMode, mode);
-  return new File(f.fd);
+  const fd = await nodeOpen(path, flagMode, mode);
+  return new File(fd);
 };
