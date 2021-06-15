b83385237b60621bcbe1cc8d5bc5c88bc36ee852
Muthu Kumar <muthukumar@thefeathers.in>
2021-06-15 19:49:39 +0530

    (feat) replace copy with Deno.copy from source
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- src/deno/stable/functions/copy.ts
+++ src/deno/stable/functions/copy.ts
@@ -1,17 +1,23 @@
 ///<reference path="../lib.deno.d.ts" />
 
-import { iter } from "../../std/io/util";
+import { DEFAULT_BUFFER_SIZE } from "../../internal/consts";
 
-export const copy: typeof Deno.copy = async (
-  src,
-  dst,
-  { bufSize = undefined } = {},
-) => {
-  let bytes = 0;
-
-  for await (const chunk of iter(src, { bufSize })) {
-    bytes += await dst.write(chunk);
+export const copy: typeof Deno.copy = async function copy(src, dst, options) {
+  let n = 0;
+  const bufSize = options?.bufSize ?? DEFAULT_BUFFER_SIZE;
+  const b = new Uint8Array(bufSize);
+  let gotEOF = false;
+  while (gotEOF === false) {
+    const result = await src.read(b);
+    if (result === null) {
+      gotEOF = true;
+    } else {
+      let nwritten = 0;
+      while (nwritten < result) {
+        nwritten += await dst.write(b.subarray(nwritten, result));
+      }
+      n += nwritten;
+    }
   }
-
-  return bytes;
+  return n;
 };
