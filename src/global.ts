44600ab6d84d6cf27fd0c6187ea72038083c26fa
Thomas Rory Gummerson <thomas@gummerson.no>
2021-08-06 09:22:30 +0200

    Update deps, export other Deno globals from main entrypoint (allow importing fetch, Response, Blob, etc)

--- src/global.ts
+++ src/global.ts
@@ -1,25 +1,8 @@
-import Blob = require("fetch-blob");
-import { Readable } from "stream";
-import * as crypto from "crypto";
-import fetch, { Headers, Request, Response } from "node-fetch";
-import { Deno } from "./index.js";
-import { confirm } from "./util/confirm.js";
-import { prompt } from "./util/prompt.js";
+import * as globals from "./index.js";
 
 Object.assign(
   globalThis,
   Object.fromEntries(
-    Object.entries({
-      crypto: (crypto as unknown as { webcrypto: Crypto }).webcrypto,
-      fetch,
-      Blob,
-      //TODO: find a WHATWG ReadableStream shim
-      Headers,
-      Request,
-      Response,
-      Deno,
-      confirm,
-      prompt,
-    }).filter(([k]) => !(k in globalThis))
-  )
+    Object.entries(globals).filter(([k]) => !(k in globalThis)),
+  ),
 );
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+export { webcrypto as crypto } from "crypto";
+export { default as Blob } from "fetch-blob";
+export { default as File } from "fetch-blob/file.js";
+export { default as fetch, Headers, Request, Response } from "node-fetch";
 export * as Deno from "./deno.js";
+export { confirm } from "./util/confirm.js";
+export { prompt } from "./util/prompt.js";
