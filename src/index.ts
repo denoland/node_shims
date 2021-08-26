4b6f30679c7485b9309c613d03a682088116af0e
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-08-26 11:13:07 +0200

    Upgrade to `undici@4.5.0`

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 export { Blob } from "buffer";
 export { webcrypto as crypto } from "crypto";
 // TODO: Wait for node builtin WhatWG Streams or fetch-blob to export them without assigning to globalThis
-export { default as fetch, Headers, Request, Response } from "undici-fetch";
+export { fetch, File, FormData, Headers, Request, Response } from "undici";
 export * as Deno from "./deno.js";
 export { confirm } from "./util/confirm.js";
 export { prompt } from "./util/prompt.js";
-export { File, FormData } from "formdata-node";
