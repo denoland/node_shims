58c25ee1588099aa9fef88a3d5ea3e78e855d10d
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-09-05 21:48:52 +0200

    Use `@fromdeno/test`

--- src/deno/stable/functions/test.ts
+++ src/deno/stable/functions/test.ts
@@ -1,35 +1,7 @@
 ///<reference path="../lib.deno.d.ts" />
 
-export const tests: Required<Deno.TestDefinition>[] = [];
-
-export const test: typeof Deno.test = function test(
-  t: string | Deno.TestDefinition,
-  fn?: () => void | Promise<void>,
-) {
-  const defaults = {
-    ignore: false,
-    only: false,
-    sanitizeOps: true,
-    sanitizeResources: true,
-    sanitizeExit: true,
-    permissions: null,
-  };
-
-  if (typeof t === "string") {
-    if (typeof fn !== "function") {
-      throw new TypeError("Missing test function");
-    }
-    if (!t) {
-      throw new TypeError("The test name can't be empty");
-    }
-    tests.push({ fn: fn, name: t, ...defaults });
-  } else {
-    if (!t.fn) {
-      throw new TypeError("Missing test function");
-    }
-    if (!t.name) {
-      throw new TypeError("The test name can't be empty");
-    }
-    tests.push({ ...defaults, ...t });
-  }
-};
+export { test, tests } from "@fromdeno/test";
+import type { AssertTrue, IsExact } from "conditional-type-checks";
+type _TypeTest = AssertTrue<
+  IsExact<typeof import("@fromdeno/test").test, typeof Deno.test>
+>;
