66d57051c402c4c7c6e0ae5b57042cc5a4edfe3b
Muthu Kumar <muthukumar@thefeathers.in>
2021-10-09 05:06:34 +0530

    fix: rm conflicting AbortController
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- tools/denolib.ts
+++ tools/denolib.ts
@@ -19,9 +19,10 @@ declare class AbortController {
   /** Returns the AbortSignal object associated with this object. */
   readonly signal: AbortSignal;
   /** Invoking this method will set this object's AbortSignal's aborted flag and
-    * signal to any observers that the associated activity is to be aborted. */
+   * signal to any observers that the associated activity is to be aborted. */
   abort(): void;
 }
+
 `,
     "",
   ),
