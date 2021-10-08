e1b2480ea42e385d666e5949e706d832238e9f0a
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-10-08 18:41:28 +0200

    Drop testing API to support upcoming `@fromdeno/test` 0.2.0
    It's probably my last commit here. Bittersweet.

--- src/deno/stable/functions/test.ts
+++ src/deno/stable/functions/test.ts
@@ -1,6 +1,6 @@
 ///<reference path="../lib.deno.d.ts" />
 
-export { test, tests } from "@fromdeno/test";
+export { test } from "@fromdeno/test";
 import type { AssertTrue, IsExact } from "conditional-type-checks";
 type _TypeTest = AssertTrue<
   IsExact<typeof import("@fromdeno/test").test, typeof Deno.test>
