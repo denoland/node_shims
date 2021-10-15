f19af0369ba5e484fbe379cf07c970f8c7986546
David Sherret <dsherret@gmail.com>
2021-10-15 15:52:03 -0400

    Add testDefinitions export.

--- src/deno/stable/functions/test.ts
+++ src/deno/stable/functions/test.ts
@@ -1,6 +1,26 @@
-///<reference path="../lib.deno.d.ts" />
+/// <reference path="../lib.deno.d.ts" />
+
+import { testDefinitions } from "../../../test.js";
 
 export const test: typeof Deno.test = function test() {
-  // no-op for now, this should be developed out within
-  // the deno shim in order to not require a peer dependency
+  let testDef: Deno.TestDefinition;
+  const firstArg = arguments[0];
+  const secondArg = arguments[1];
+
+  if (typeof firstArg === "string") {
+    if (typeof secondArg !== "function") {
+      throw new TypeError("Missing test function");
+    }
+    testDef = { name: firstArg, fn: secondArg };
+  } else {
+    if (!firstArg?.name) {
+      throw new TypeError("Missing test name");
+    }
+    if (!firstArg?.fn) {
+      throw new TypeError("Missing test function");
+    }
+    testDef = { ...firstArg };
+  }
+
+  testDefinitions.push(testDef);
 };
