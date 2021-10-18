ac904213538aa75140959b6f6f71735e6fc3b70b
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-10-18 12:23:54 +0200

    Make `@fromdeno/test` optional

--- src/deno/stable/functions/test.ts
+++ src/deno/stable/functions/test.ts
@@ -1,6 +1,13 @@
 ///<reference path="../lib.deno.d.ts" />
 
-export { test } from "@fromdeno/test";
+export let test: typeof Deno.test;
+
+try {
+  test = require("@fromdeno/test").test;
+} catch {
+  test = () => {};
+}
+
 import type { AssertTrue, IsExact } from "conditional-type-checks";
 type _TypeTest = AssertTrue<
   IsExact<typeof import("@fromdeno/test").test, typeof Deno.test>
