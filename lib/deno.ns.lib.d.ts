3d76dddaff93fc018e7369008366209db0ea10dd
David Sherret <dsherret@users.noreply.github.com>
2021-11-04 10:53:42 -0400

    fix: restore errors namespace in declaration file (#45)

--- lib/deno.ns.lib.d.ts
+++ lib/deno.ns.lib.d.ts
@@ -2455,58 +2455,60 @@ export declare namespace Deno {
         toObject(): { [index: string]: string };
       };
 
-  export class AddrInUse extends Error {
-  }
+  export namespace errors {
+    export class AddrInUse extends Error {
+    }
 
-  export class AddrNotAvailable extends Error {
-  }
+    export class AddrNotAvailable extends Error {
+    }
 
-  export class AlreadyExists extends Error {
-  }
+    export class AlreadyExists extends Error {
+    }
 
-  export class BadResource extends Error {
-  }
+    export class BadResource extends Error {
+    }
 
-  export class BrokenPipe extends Error {
-  }
+    export class BrokenPipe extends Error {
+    }
 
-  export class Busy extends Error {
-  }
+    export class Busy extends Error {
+    }
 
-  export class ConnectionAborted extends Error {
-  }
+    export class ConnectionAborted extends Error {
+    }
 
-  export class ConnectionRefused extends Error {
-  }
+    export class ConnectionRefused extends Error {
+    }
 
-  export class ConnectionReset extends Error {
-  }
+    export class ConnectionReset extends Error {
+    }
 
-  export class Http extends Error {
-  }
+    export class Http extends Error {
+    }
 
-  export class Interrupted extends Error {
-  }
+    export class Interrupted extends Error {
+    }
 
-  export class InvalidData extends Error {
-  }
+    export class InvalidData extends Error {
+    }
 
-  export class NotConnected extends Error {
-  }
+    export class NotConnected extends Error {
+    }
 
-  export class NotFound extends Error {
-  }
+    export class NotFound extends Error {
+    }
 
-  export class PermissionDenied extends Error {
-  }
+    export class PermissionDenied extends Error {
+    }
 
-  export class TimedOut extends Error {
-  }
+    export class TimedOut extends Error {
+    }
 
-  export class UnexpectedEof extends Error {
-  }
+    export class UnexpectedEof extends Error {
+    }
 
-  export class WriteZero extends Error {
+    export class WriteZero extends Error {
+    }
   }
 
   /** The URL of the entrypoint module entered from the command-line. */
