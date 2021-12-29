2429997194e51560bd6bee19584bcdcbcfcd63a4
Tilo <tilo.codes@gmail.com>
2021-12-29 15:34:17 +0100

    fmt

--- packages/shim-deno/src/deno/stable/functions/open.test.ts
+++ packages/shim-deno/src/deno/stable/functions/open.test.ts
@@ -1,32 +1,37 @@
-import { open } from "./open.ts"
+import { open } from "./open.ts";
 import assert from "assert/strict";
 import path from "path";
 
-const withTempDir = (test: (tempDirPath: string) => Promise<void>) => async () => {
+const withTempDir = (test: (tempDirPath: string) => Promise<void>) =>
+  async () => {
     const tempDirPath = await Deno.makeTempDir();
     try {
-        await test(tempDirPath);
+      await test(tempDirPath);
     } finally {
-        await Deno.remove(tempDirPath, { recursive: true });
+      await Deno.remove(tempDirPath, { recursive: true });
     }
-}
+  };
 
-Deno.test("creates file when createNew is set to true and file does not exist",
-    withTempDir(async (tempDirPath) => {
-        const filePath = path.join(tempDirPath, "some")
+Deno.test(
+  "creates file when createNew is set to true and file does not exist",
+  withTempDir(async (tempDirPath) => {
+    const filePath = path.join(tempDirPath, "some");
 
-        const fileHandle = await open(filePath, { createNew: true, write: true });
-        fileHandle.close();
+    const fileHandle = await open(filePath, { createNew: true, write: true });
+    fileHandle.close();
 
-        assert.ok(await Deno.stat(filePath));
-    }));
+    assert.ok(await Deno.stat(filePath));
+  }),
+);
 
-Deno.test("errors when createNew is set to true and file exists", withTempDir(async (tempDirPath) => {
-
+Deno.test(
+  "errors when createNew is set to true and file exists",
+  withTempDir(async (tempDirPath) => {
     const testFile = await Deno.makeTempFile({ dir: tempDirPath });
 
     await assert.rejects(
-        open(testFile, { createNew: true, write: true }),
-        { message: `EEXIST: file already exists, open '${testFile}'` }
+      open(testFile, { createNew: true, write: true }),
+      { message: `EEXIST: file already exists, open '${testFile}'` },
     );
-}));
+  }),
+);
