cfdbddc370ac1b5ebb48f9d29c562f5c106678ba
Wojciech Pawlik <woj.pawlik@gmail.com>
2022-01-07 21:21:02 +0100

    fix: polyfill `EventTarget` so the deno shim doesn't crash on load in Node 14 (#80)

--- packages/shim-deno/src/deno/stable/classes/PermissionStatus.ts
+++ packages/shim-deno/src/deno/stable/classes/PermissionStatus.ts
@@ -3,6 +3,8 @@
 // The listeners don't actually matter because the state of these permissions
 // is constant and mocked as Node.js has all doors open.
 
+(globalThis as any).EventTarget ??= require("events").EventTarget ?? null;
+
 export class PermissionStatus extends EventTarget
   implements Deno.PermissionStatus {
   onchange: ((this: PermissionStatus, ev: Event) => any) | null = null;
