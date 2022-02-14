6daf90daa2043c3e2b9b7c973997899bf37e9185
David Sherret <dsherret@gmail.com>
2022-02-14 11:45:58 -0500

    Use export * instead

--- packages/shim-deno-test/src/index.ts
+++ packages/shim-deno-test/src/index.ts
@@ -1,8 +1,3 @@
 export * as Deno from "./test.js";
-export {
-  test,
-  TestContext,
-  TestDefinition,
-  TestStepDefinition,
-} from "./test.js";
+export * from "./test.js";
 export { testDefinitions } from "./definitions.js";
