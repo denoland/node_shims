bd4a83ef3fb0788541191e4a089563c63da6cd1f
Yongwook Choi <yongwook.choi@riiid.co>
2022-03-12 03:38:17 +0900

    fix(shim-deno/std): fix std not to use FsFile (#98)
    FsFile would error when someone imported the node "ps" module.

--- packages/shim-deno/src/deno/stable/variables/std.ts
+++ packages/shim-deno/src/deno/stable/variables/std.ts
@@ -1,7 +1,101 @@
 ///<reference path="../lib.deno.d.ts" />
 
-import { File } from "../classes/FsFile.js";
+function chain<T extends (...args: any[]) => Promise<any>>(
+  fn: T,
+  cleanup?: () => void,
+): T {
+  let prev: Promise<any> | undefined;
+  return function _fn(...args) {
+    const curr = (prev || Promise.resolve())
+      .then(() => fn(...args))
+      .finally(cleanup || (() => {}))
+      .then((result) => {
+        if (prev === curr) prev = undefined;
+        return result;
+      });
+    return (prev = curr);
+  } as T;
+}
 
-export const stdin: typeof Deno.stdin = new File(0);
-export const stdout: typeof Deno.stdout = new File(1);
-export const stderr: typeof Deno.stderr = new File(2);
+export const stdin: typeof Deno.stdin = {
+  rid: 0,
+  read: chain(
+    (p) => {
+      return new Promise((resolve, reject) => {
+        process.stdin.resume();
+        process.stdin.on("error", onerror);
+        process.stdin.once("readable", () => {
+          process.stdin.off("error", onerror);
+          const data = process.stdin.read(p.length) ?? process.stdin.read();
+          if (data) {
+            p.set(data);
+            resolve(data.length > 0 ? data.length : null);
+          } else {
+            resolve(null);
+          }
+        });
+        function onerror(error: Error) {
+          reject(error);
+          process.stdin.off("error", onerror);
+        }
+      });
+    },
+    () => process.stdin.pause(),
+  ),
+  get readable(): ReadableStream<Uint8Array> {
+    throw new Error("Not implemented.");
+  },
+  readSync() {
+    // Node.js doesn't support readSync for stdin
+    throw new Error("Not implemented.");
+  },
+  close() {
+    process.stdin.destroy();
+  },
+};
+export const stdout: typeof Deno.stdout = {
+  rid: 1,
+  write: chain((p) => {
+    return new Promise((resolve) => {
+      const result = process.stdout.write(p);
+      if (!result) {
+        process.stdout.once("drain", () => resolve(p.length));
+      } else {
+        resolve(p.length);
+      }
+    });
+  }),
+  get writable(): WritableStream<Uint8Array> {
+    throw new Error("Not implemented.");
+  },
+  writeSync() {
+    // Node.js doesn't support writeSync for stdout
+    throw new Error("Not implemented");
+  },
+  close() {
+    process.stdout.destroy();
+  },
+};
+export const stderr: typeof Deno.stderr = {
+  rid: 2,
+  write: chain((p) => {
+    return new Promise((resolve) => {
+      const result = process.stderr.write(p);
+      if (!result) {
+        process.stderr.once("drain", () => resolve(p.length));
+      } else {
+        resolve(p.length);
+      }
+    });
+  }),
+  get writable(): WritableStream<Uint8Array> {
+    throw new Error("Not implemented.");
+  },
+  writeSync() {
+    // Node.js doesn't support writeSync for stderr
+    throw new Error("Not implemented");
+  },
+  close() {
+    process.stderr.destroy();
+  },
+};
