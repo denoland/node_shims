ec4efdd215971ae6331f073978f6e2ea91fff119
UrielCh <uriel.chemouni@gmail.com>
2022-07-21 20:27:23 +0300

    fmt + lint

--- packages/shim-deno/src/deno/stable/functions/resolveDns.test.ts
+++ packages/shim-deno/src/deno/stable/functions/resolveDns.test.ts
@@ -2,11 +2,11 @@ import assert from "assert/strict";
 import { resolveDns } from "./resolveDns";
 
 Deno.test("resolve A as ipV4 string", async () => {
-    const ips = await resolveDns('www.github.com', 'A');
-    assert(ips[0].match(/(\d+\.){3}\d+/), "Must be a IP v4")
+  const ips = await resolveDns("www.github.com", "A");
+  assert(ips[0].match(/(\d+\.){3}\d+/), "Must be a IP v4");
 });
 
 Deno.test("resolve A as ipV6 string", async () => {
-    const ips = await resolveDns('www.google.com', 'AAAA');
-    assert(ips[0].match(/[0-9a-f]{1,4}:[0-9a-f:]+/), "Must be a IP v6")
+  const ips = await resolveDns("www.google.com", "AAAA");
+  assert(ips[0].match(/[0-9a-f]{1,4}:[0-9a-f:]+/), "Must be a IP v6");
 });
--- packages/shim-deno/src/deno/stable/functions/resolveDns.ts
+++ packages/shim-deno/src/deno/stable/functions/resolveDns.ts
@@ -2,37 +2,38 @@
 
 import dns from "dns";
 
-export const resolveDns: typeof Deno.resolveDns = async function resolveDns(
-    query,
-    recordType,
-    options,
+export const resolveDns: typeof Deno.resolveDns = function resolveDns(
+  query,
+  recordType,
+  options,
 ): Promise<any[]> {
-    if (options)
-        throw Error(`resolveDns option not implemnted yet`);
-    switch (recordType) {
-        case "A":
-        case "AAAA":
-        // case "ANAME":
-        case "CNAME":
-        case "NS":
-        case "PTR":
-            return new Promise((resolve, reject) => {
-                dns.resolve(query, recordType, (err, addresses) => {
-                    if (err)
-                        reject(err);
-                    else
-                        resolve(addresses as string[]);
-                });
-            });
-        case "ANAME":
-        case "CAA":
-        case "MX":
-        case "NAPTR":
-        case "SOA":
-        case "SRV":
-        case "TXT":
-        default:
-            throw Error(`resolveDns type ${recordType} not implemnted yet`);
-    }
-    return [] as string[];
+  if (options) {
+    throw Error(`resolveDns option not implemnted yet`);
+  }
+  switch (recordType) {
+    case "A":
+    /* falls through */
+    case "AAAA":
+    case "CNAME":
+    case "NS":
+    case "PTR":
+      return new Promise((resolve, reject) => {
+        dns.resolve(query, recordType, (err, addresses) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(addresses as string[]);
+          }
+        });
+      });
+    case "ANAME":
+    case "CAA":
+    case "MX":
+    case "NAPTR":
+    case "SOA":
+    case "SRV":
+    case "TXT":
+    default:
+      throw Error(`resolveDns type ${recordType} not implemnted yet`);
+  }
 };
