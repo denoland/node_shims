f0d7ae73297e6e2c81ca84a0a08142ba654ea6fd
UrielCh <uriel.chemouni@gmail.com>
2022-07-22 19:28:51 +0300

    enable test

--- packages/shim-deno/src/deno/stable/functions/resolveDns.test.ts
+++ packages/shim-deno/src/deno/stable/functions/resolveDns.test.ts
@@ -1,5 +1,11 @@
 import assert from "assert/strict";
-import { resolveDns } from "./resolveDns";
+import { resolveDns } from "./resolveDns.js";
+
+/**
+ * A reference to this test in
+ * packages/shim-deno/tools/working_test_files.txt
+ * activate this test file.
+ */
 
 Deno.test("resolve A as ipV4 string", async () => {
   const ips = await resolveDns("www.github.com", "A");
