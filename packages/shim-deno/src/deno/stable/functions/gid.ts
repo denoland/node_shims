56931c7f6e434533ed4bf838096ed8812d3c80e4
Deniz <denizdogan@users.noreply.github.com>
2023-05-09 21:23:25 +0200

    feat: add gid and uid
    Fixes #137

---
+++ packages/shim-deno/src/deno/stable/functions/gid.ts
@@ -0,0 +1,5 @@
+/// <reference path="../lib.deno.d.ts" />
+
+import ps from "process";
+
+export const gid: typeof Deno.gid = ps.getgid;
---
+++ packages/shim-deno/src/deno/stable/functions/uid.ts
@@ -0,0 +1,5 @@
+/// <reference path="../lib.deno.d.ts" />
+
+import ps from "process";
+
+export const uid: typeof Deno.uid = ps.getuid;
