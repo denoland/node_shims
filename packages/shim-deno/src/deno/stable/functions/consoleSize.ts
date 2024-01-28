ea004cf688dd7d94da4849e85bb35b59fffbaf8d
David Sherret <dsherret@gmail.com>
2024-01-28 13:22:13 -0500

    fix: Deno.consoleSize should handle if stdout is piped

--- packages/shim-deno/src/deno/stable/functions/consoleSize.ts
+++ packages/shim-deno/src/deno/stable/functions/consoleSize.ts
@@ -1,6 +1,13 @@
 ///<reference path="../lib.deno.d.ts" />
 
 export const consoleSize: typeof Deno.consoleSize = function consoleSize() {
-  const { columns, rows } = process.stdout;
-  return { columns, rows };
+  const pipes = [process.stderr, process.stdout];
+  for (const pipe of pipes) {
+    if (pipe.columns != null) {
+      const { columns, rows } = pipe;
+      return { columns, rows };
+    }
+  }
+  // both stdout and stderr were piped (not the best Error, but it's what Deno does)
+  throw new Error("The handle is invalid.");
 };
