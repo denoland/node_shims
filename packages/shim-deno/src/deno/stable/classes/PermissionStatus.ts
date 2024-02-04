6439d0363300ad4f249edc7b0084b147e6d16896
David Sherret <dsherret@gmail.com>
2024-02-04 13:47:50 -0500

    refactor(BREAKING): drop Node 14 support

--- packages/shim-deno/src/deno/stable/classes/PermissionStatus.ts
+++ packages/shim-deno/src/deno/stable/classes/PermissionStatus.ts
@@ -1,10 +1,5 @@
 ///<reference path="../lib.deno.d.ts" />
 
-// The listeners don't actually matter because the state of these permissions
-// is constant and mocked as Node.js has all doors open.
-
-(globalThis as any).EventTarget ??= require("events").EventTarget ?? null;
-
 export class PermissionStatus extends EventTarget
   implements Deno.PermissionStatus {
   onchange: ((this: PermissionStatus, ev: Event) => any) | null = null;
