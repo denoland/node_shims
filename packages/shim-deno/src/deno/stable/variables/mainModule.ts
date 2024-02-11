bdfcce267f7c98153c213a6a98c2458f9f3e23f1
David Sherret <dsherret@gmail.com>
2024-02-10 21:07:10 -0500

    fix: correct mainModule code

--- packages/shim-deno/src/deno/stable/variables/mainModule.ts
+++ packages/shim-deno/src/deno/stable/variables/mainModule.ts
@@ -3,5 +3,5 @@ import { join } from "path";
 import { pathToFileURL } from "url";
 
 export const mainModule: typeof Deno.mainModule = pathToFileURL(
-  process.argv[1] ?? join(Deno.cwd(), "$deno$repl.ts"),
+  process.argv[1] ?? join(process.cwd(), "$deno$repl.ts"),
 ).href;
